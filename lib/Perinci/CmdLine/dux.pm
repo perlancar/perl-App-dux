b6751925d059be3a17cced89eb7c2289dadb804a
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-04-12 10:34:59 +0700

    Observe metadata x.dux.default_format

--- lib/Perinci/CmdLine/dux.pm
+++ lib/Perinci/CmdLine/dux.pm
@@ -8,11 +8,18 @@ sub run_subcommand {
     require Tie::Diamond;
 
     my $self = shift;
-    my $chomp = $self->{_meta}{"x.dux.strip_newlines"} // 1;
 
+    # set `in` and `out` arguments for the dux function
+    my $chomp = $self->{_meta}{"x.dux.strip_newlines"} // 1;
     tie my(@diamond), 'Tie::Diamond', {chomp=>$chomp} or die;
     $self->{_args}{in}  = \@diamond;
     $self->{_args}{out} = [];
+
+    # set default output format from metadata, if specified and user has not
+    # specified --format
+    my $mfmt = $self->{_meta}{"x.dux.default_format"};
+    $self->format($mfmt) unless
+        grep {/^--format/} @{ $self->{_orig_argv} }; # not a proper way, but will do for now
 
     $self->SUPER::run_subcommand(@_);
 }
