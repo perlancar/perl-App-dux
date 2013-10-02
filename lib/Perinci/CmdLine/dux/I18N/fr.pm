96aec1f4aa8882c269e7e6d9dbbdd200318c082c
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-10-02 21:17:45 +0700

    Add (empty) French translation

---
+++ lib/Perinci/CmdLine/dux/I18N/fr.pm
@@ -0,0 +1,18 @@
+use utf8;
+
+package Perinci::CmdLine::dux::I18N::fr;
+use parent qw(Perinci::CmdLine::dux::I18N Perinci::CmdLine::I18N::fr);
+
+use Locale::Maketext::Lexicon::Gettext;
+our %Lexicon = %{ Locale::Maketext::Lexicon::Gettext->parse(<DATA>) };
+
+# VERSION
+
+#use Data::Dump; dd \%Lexicon;
+
+1;
+# ABSTRACT: French translation for Perinci::CmdLine::dux
+__DATA__
+
+msgid  ""
+msgstr ""
